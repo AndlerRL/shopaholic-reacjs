5e14e5176d7f088a089e457c0e189efce2442e8b
Meynor R Lucas <andre.rlucas@outlook.com>
2019-05-10 14:02:07 -0600

    After eject [v0.0.1]

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './App.css';
+
+function App() {
+  return (
+    <div className={css.App}>
+      <h1>App container</h1>
+    </div>
+  );
+}
+
+export default App;
